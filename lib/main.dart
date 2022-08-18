185e5e692f03b3b02a62be99df12d9fb738a2b21
Anthonychibuike <Anthonychibuike246@gmail.com>
2022-08-18 12:18:39 +0100

    initialize firebase in project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:instagram/responsive/responsive_layout_screen.dart';
+import 'package:instagram/screens/mobile_screen.dart';
+import 'package:instagram/screens/web_screen.dart';
+import 'package:instagram/utils/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  if(Platform.isIOS){
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  }else{
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
@@ -11,10 +26,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Instagram',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+      home: const ResponsiveLayoutScreen(mobileScreenLayout: MobileScreen(),webScreenLayout: WebScreen(),),
     );
   }
 }
